ede59c2889e6610a92c41ec0ad2a56685690b4bb
Herb McNew <hjmcnew@github.com>
2025-02-01 13:25:01 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,50 @@
+input.calibrateCompass()
 basic.forever(function () {
-	
+    if (input.compassHeading() < 45) {
+        basic.showLeds(`
+            # . . . #
+            # # . . #
+            # . # . #
+            # . . # #
+            # . . . #
+            `)
+    } else {
+        if (input.compassHeading() < 135) {
+            basic.showLeds(`
+                # # # # #
+                # . . . .
+                # # # # .
+                # . . . .
+                # # # # #
+                `)
+        } else {
+            if (input.compassHeading() < 225) {
+                basic.showLeds(`
+                    . # # # #
+                    # . . . .
+                    . # # # .
+                    . . . . #
+                    # # # # .
+                    `)
+            } else {
+                if (input.compassHeading() < 315) {
+                    basic.showLeds(`
+                        # . . . #
+                        # . . . #
+                        # . # . #
+                        # # . # #
+                        # . . . #
+                        `)
+                } else {
+                    basic.showLeds(`
+                        # . . . #
+                        # # . . #
+                        # . # . #
+                        # . . # #
+                        # . . . #
+                        `)
+                }
+            }
+        }
+    }
 })
